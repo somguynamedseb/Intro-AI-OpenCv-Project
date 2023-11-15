0c882982c68b541acf73a3ba12345ce2b58bf8b3
Willem <willemjohnvo@gmail.com>
2023-11-15 17:57:12 -0500

    removes faces that are too large

--- both.py
+++ both.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 import cv2 as cv
 import numpy as np
 
@@ -15,13 +16,31 @@ result, panorama = stitcher.stitch((img1, img2, img3))
 # Check if stitching was successful
 if result == cv.Stitcher_OK:
     # Facial recognition on the stitched panorama
-    gray_panorama = cv.cvtColor(panorama, cv.COLOR_BGR2GRAY)
-    haar_cascade = cv.CascadeClassifier('haar_face.xml')
-    faces_rect = haar_cascade.detectMultiScale(gray_panorama, scaleFactor=1.1, minNeighbors=2)
 
+    # Load the Haar Cascade classifier for face detection
+    haar_cascade = cv.CascadeClassifier('haar_face.xml')
+
+    # Detect faces in the panorama using the Haar Cascade
+    faces_rect = haar_cascade.detectMultiScale(panorama, scaleFactor=1.1, minNeighbors=2)
+
+    # Print the number of faces found in the panorama
     print(f'Number of faces found in the panorama = {len(faces_rect)}')
 
-    for i, (x, y, w, h) in enumerate(faces_rect):
+    # Calculate the average size of detected faces
+    face_sizes = np.array([w * h for _, _, w, h in faces_rect])
+    avg_face_size = np.mean(face_sizes)
+
+    # Define a threshold for filtering outliers
+    threshold = 1  # You can adjust this threshold based on your needs
+
+    # Filter out faces that deviate significantly from the average size
+    filtered_faces_rect = [rect for rect in faces_rect if abs(rect[2] * rect[3] - avg_face_size) < threshold * avg_face_size]
+
+    # Print the number of faces after filtering
+    print(f'Number of filtered faces = {len(filtered_faces_rect)}')
+
+    # Draw rectangles around the filtered faces and annotate with numbers
+    for i, (x, y, w, h) in enumerate(filtered_faces_rect):
         cv.rectangle(panorama, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
         cv.putText(panorama, f'{i + 1}', (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
@@ -30,4 +49,5 @@ if result == cv.Stitcher_OK:
     cv.waitKey()
     cv.destroyAllWindows()
 else:
+    # Print an error message if stitching was not successful
     print('Error during stitching')
