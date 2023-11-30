4a83ce263740db58f4879b6ddf38abaea9614f07
Hunter102 <40743716+Hunter102@users.noreply.github.com>
2023-11-29 20:32:53 -0500

    Update stitching.py

--- stitching.py
+++ stitching.py
@@ -1,21 +1,26 @@
 import cv2 as cv
-import numpy as np
 
 # Load images for panorama stitching
 img1 = cv.imread('IMG_4071.jpg')
 img2 = cv.imread('IMG_4072.jpg')
 img3 = cv.imread('IMG_4073.jpg')
 
+print(img1.shape)
+print(img2.shape)
+print(img3.shape)
+
+
 # Create a stitcher object
-stitcher = cv.Stitcher_create()
+stitcher = cv.Stitcher.create()
 
 # Stitch the images
 result, panorama = stitcher.stitch((img1, img2, img3))
 
 # Check if stitching was successful
 if result == cv.Stitcher_OK:
+    print(panorama)
     cv.imshow('Panorama', panorama)
-    cv.waitKey()
+    cv.waitKey(0)
     cv.destroyAllWindows()
 else:
     print('Error during stitching')
