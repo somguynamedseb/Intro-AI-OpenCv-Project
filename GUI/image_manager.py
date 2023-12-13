c7662817dec99e471786dc7845f5926fbf695d0e
Sebastian Baldini <sebastian.baldini@outlook.com>
2023-12-13 14:25:11 -0500

    merged changes

--- GUI/image_manager.py
+++ GUI/image_manager.py
@@ -2,6 +2,8 @@ import os
 from ultralytics import YOLO
 from PIL import Image
 import cv2 as cv
+import io
+import PySimpleGUI as sg
 
 model = YOLO("runs/detect/train4/weights/last.pt")
 
@@ -19,8 +21,40 @@ class image_manager:
     def clear_images(self):
         self.img_arr = []
 
-    def stich_images(self)->[str,int]: ##returns DIR of stiched img
-        raise NotImplementedError
+    def stich_images(self,window)->[str]: ##returns DIR of stiched img
+       # Read images and store them in a list
+        images = self.img_arr
+        # Create a stitcher object
+        stitcher = cv.Stitcher.create()
+        # Perform the stitching process
+        status, stitched_img = stitcher.stitch(images)
+        if status == cv.Stitcher_OK:
+            # Convert the stitched image to a PIL Image
+            stitched_pil_image = Image.fromarray(cv.cvtColor(stitched_img, cv.COLOR_BGR2RGB))
+            # Define the size to which you want to scale the image
+            max_width, max_height = 800, 600  # Adjust these values as needed
+            # Calculate the scaling factor, maintaining the aspect ratio
+            scaling_factor = min(max_width / stitched_pil_image.width, max_height / stitched_pil_image.height)
+            # Compute the new size
+            new_size = (int(stitched_pil_image.width * scaling_factor),
+                        int(stitched_pil_image.height * scaling_factor))
+            # Resize the image
+            stitched_pil_image = stitched_pil_image.resize(new_size, Image.Resampling.LANCZOS)
+            # Save the resized image to a BytesIO object
+            bio = io.BytesIO()
+            stitched_pil_image.save(bio, format="PNG")
+            bio.seek(0)
+            # Update the GUI to display the resized stitched image
+            img = bio.read()
+            window["-IMAGE-"].update(data=img)
+            window[f'-IMGTEXT-'].update("Stitched Image:")
+            window[f'-TOUT-'].update("")
+            window["-SKIP-"].update(visible=False)
+            window[f'-NEXT-'].update(visible=True)
+            return img
+        else:
+            sg.popup_error('Image stitching failed!', 'Error code: ' + str(status))
+            return None
 
     #detects faces within stored images
     def detect_faces(self)->str: ##returns DIR of output img
